20d6c40023850ee382439b21ed6f6001587a49b8
acrowe1 <ambergcrowe@gmail.com>
2024-04-22 16:46:19 -0500

    got controller/sound to work; can't draw yet

---
+++ controllerDraw.js
@@ -0,0 +1,158 @@
+let port;
+let joyX = 0, joyY = 0, sw = 0;
+let connectButton;
+let circleX, circleY; 
+let speed = 0.005; 
+let dragging = false;
+let colors;
+let selectedColor;
+const size = 10;
+let synth;
+let clearButton;
+let saveButton;
+
+function setup() {
+  port = createSerial(); 
+  createCanvas(1000, 1000);
+  circleX = width / 2; 
+  circleY = height / 2; 
+  selectedColor = color('white');
+  synth = new Tone.Synth().toDestination();
+
+  colors = [new ColorSquare(0, 0, color('red')),
+            new ColorSquare(0, 20, color('orange')),
+            new ColorSquare(0, 40, color('yellow')),
+            new ColorSquare(0, 60, color('limegreen')),
+            new ColorSquare(0, 80, color('lightblue')),
+            new ColorSquare(0, 100, color('blue')),
+            new ColorSquare(0, 120, color('magenta')),
+            new ColorSquare(0, 140, color('brown')),
+            new ColorSquare(0, 160, color('white')),
+            new ColorSquare(0, 180, color('black'))];
+
+  clearButton = createButton('Clear');
+  clearButton.position(1, 200);
+  clearButton.mousePressed(clearCanvas);
+
+  saveButton = createButton('Save');
+  saveButton.position(1, 225);
+  saveButton.mousePressed(saveCanvasToFile);
+
+  connectButton = createButton("Connect");
+  connectButton.position(1, 250);
+  connectButton.mousePressed(connect);
+
+  let usedPorts = usedSerialPorts(); 
+  if (usedPorts.length > 0) {
+    port.open(usedPorts[0], 57600);
+  }
+  frameRate(90);
+}
+
+function startToneContext() {
+  Tone.start();
+}
+
+function draw() {
+  background(220);
+
+  let characters = port.available(); 
+  if (characters > 0) {
+    let str = port.read(characters);
+    let lines = str.split("\n"); 
+    let latest = ""; 
+    if (lines.length > 0) {
+      let lastIndex = lines.length > 1 ? lines.length-2 : lines.length-1;
+      latest = lines[lastIndex];
+    }
+
+    let values = latest.split(",");
+    if (values.length > 2) {
+      joyX = Number(values[0]); 
+      joyY = Number(values[1]); 
+      sw = Number(values[2]); 
+    }
+  }
+
+  // Update circle position based on joystick input
+  circleX += joyX * speed;
+  circleY += joyY * speed;
+
+  // Constrain circle position to stay within canvas boundaries
+  circleX = constrain(circleX, size / 2, width - size / 2);
+  circleY = constrain(circleY, size / 2, height - size / 2);
+
+  for (let i = 0; i < colors.length; i++) {
+    colors[i].draw();
+  }
+
+  stroke(selectedColor);
+  fill(selectedColor);
+
+  // Check if the circle is hovering over any color square
+  for (let i = 0; i < colors.length; i++) {
+    if (colors[i].isCircleOver()) {
+      selectedColor = colors[i].fill;
+      break;
+    }
+  }
+
+  if (sw == 1) {
+    dragging = true;
+    playNote();
+  } else {
+    dragging = false;
+  }
+
+  if (dragging) {
+    circle(circleX, circleY, size);
+    set(circleX, circleY, selectedColor);
+  }
+
+  circle(circleX, circleY, 10);
+}
+
+function startToneContext() {
+  Tone.start();
+}
+
+function playNote() {
+  let noteFreq = Tone.Frequency(map(circleY, 0, height, 48, 72), "midi");
+  // let velocity = map(circleX, 0, width, 0.2, 1);
+  
+  synth.triggerAttackRelease(noteFreq, "8n");
+}
+
+function clearCanvas() {
+  background(255);
+}
+
+function saveCanvasToFile() {
+  saveCanvas('beautifulWorkOfArt', 'png');
+}
+
+function connect() {
+  if (!port.opened()) {
+    port.open('Arduino', 57600);
+  } else {
+    port.close();
+  }
+}
+
+class ColorSquare {
+  constructor(x, y, fill) {
+    this.x = x;
+    this.y = y;
+    this.fill = fill;
+  }
+
+  draw() {
+    stroke(225);
+    fill(this.fill);
+    square(this.x, this.y, 20);
+  }
+
+  isCircleOver() {
+    return circleX > this.x && circleX < this.x + 20 && circleY > this.y && circleY < this.y + 20;
+  }
+}
